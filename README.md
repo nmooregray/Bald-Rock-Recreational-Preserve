5f200ca42bcc2bb4aa18999dc6072229d94716ff
Nick Gray <nmgr240@uky.edu>
2024-03-24 20:22:56 -0400

    Finished
    I did it.

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
 # Bald Rock Recreational Preserve and Wedding Barn
-This is Bald Recreational Preserve, an area owned by the Coalition adjacent to the Pendergrass-Murray Recreational Preserve. In this animation, we focus on the wedding venue siting atop the ridge overlooking the Chocolate Factory Crag and the Bald Rock Crag. 
+This is Bald Recreational Preserve, an area owned by the Coalition adjacent to the Pendergrass-Murray Recreational Preserve. In this animation, we focus on the wedding venue siting atop the ridge overlooking the Chocolate Factory Crag and the Bald Rock Cove Crag. 
 
 ![Map of the Wedding Barn](./map.jpg)
+
+![Screenshot of Animation](./animation.jpg) 
+Heres a screenshot of it!
+
+*[Bald Rock: Point Cloud](https://www.youtube.com/watch?v=gyALwbH6kuI)*
+
+[Here's](http://127.0.0.1:5500/viewer/index.html) a link to an interactive map online!
---
+++ viewer/pointclouds/index/cloud.js
@@ -0,0 +1,91 @@
+{
+    "version": "1.8",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 11366543,
+    "boundingBox": {
+        "lx": 5508169.16,
+        "ly": 3765333.5,
+        "lz": 840.0899999999999,
+        "ux": 5512169.15,
+        "uy": 3769333.49,
+        "uz": 4840.080000000224
+    },
+    "tightBoundingBox": {
+        "lx": 5508169.16,
+        "ly": 3765333.5,
+        "lz": 840.0899999999999,
+        "ux": 5512169.15,
+        "uy": 3769333.49,
+        "uz": 1293.56
+    },
+    "pointAttributes": [
+        {
+            "name": "POSITION_CARTESIAN",
+            "size": 12,
+            "elements": 3,
+            "elementSize": 4,
+            "type": "int32",
+            "description": ""
+        },
+        {
+            "name": "RGBA",
+            "size": 4,
+            "elements": 4,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "classification",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "gps-time",
+            "size": 8,
+            "elements": 1,
+            "elementSize": 8,
+            "type": "double",
+            "description": ""
+        },
+        {
+            "name": "intensity",
+            "size": 2,
+            "elements": 1,
+            "elementSize": 2,
+            "type": "uint16",
+            "description": ""
+        },
+        {
+            "name": "number of returns",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "return number",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "source id",
+            "size": 2,
+            "elements": 1,
+            "elementSize": 2,
+            "type": "uint16",
+            "description": ""
+        }
+    ],
+    "spacing": 34.64093017578125,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
